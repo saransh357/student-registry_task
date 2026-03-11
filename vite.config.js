719de407666eb76d1fc9d01bf9ca2437cb177a1c
saransh357 <120619577+saransh357@users.noreply.github.com>
2026-03-11 21:50:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite";
 import react            from "@vitejs/plugin-react";
 
-/**
- * GITHUB PAGES:
- * Set the `base` to your repo name so asset paths resolve correctly.
- * Example: if your repo is github.com/yourname/student-registry
- *          set base: "/student-registry/"
- *
- * For Vercel / Netlify, keep base: "/"
- */
+
+ // * For Vercel / Netlify, keep base: "/"
+ // */
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_PAGES ? "/student-registry_task/" : "/",
