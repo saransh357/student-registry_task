7f3ad232a1249c907dd68b6282ad72e9fe873dc7
saransh357 <120619577+saransh357@users.noreply.github.com>
2026-03-11 21:55:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react            from "@vitejs/plugin-react";
 
-
- // * For Vercel / Netlify, keep base: "/"
- // */
 export default defineConfig({
   plugins: [react()],
   base: process.env.GITHUB_PAGES ? "/student-registry_task/" : "/",
